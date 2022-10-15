adcfba2e520b2d2b95d2964173b58b57814a4e91
MutheuLinet <muneenilinet@gmail.com>
2022-10-15 08:01:50 +0300

    Add and access props in react components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import './App.css';
 import Info from './Info.js' 
+
 function App() {
   return (
     <div className="App">
     <Info />
-    <AddItem />
+    <AddItem text='linet' number={1}/>
+    <AddItem text='lynne'/>
+    <AddItem text='test'/>
     </div>
   );
 }
 
-
-function AddItem(){
-  const value="default";
+function AddItem(props){
+  const value= props.text; //accessing props,by default props are javascript objects with name and value
   return(
     <form>
       <label for="text-form">Type Something: </label>
