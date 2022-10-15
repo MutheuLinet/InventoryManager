0ed1eee402922f1506d6e5d05d48685af89d128f
MutheuLinet <muneenilinet@gmail.com>
2022-10-15 08:43:49 +0300

    Add props to Info component

--- src/App.js
+++ src/App.js
@@ -1,30 +1,31 @@
 import './App.css';
-import Info from './Info.js' 
+import Info from './Info.js'
 
 function App() {
   return (
     <div className="App">
-    <Info />
-    <AddItem text='linet' number={0}/>
-    <AddItem text='lynne'number={1}/>
-    <AddItem />
+      <Info title="Inventory Management System" />
+      <AddItem text='linet' number={0} />
+      <AddItem text='lynne' number={1} />
+      <Info />
+      <AddItem />
     </div>
   );
 }
 
-function AddItem(props){
-  const value= props.text; //accessing props,by default props are javascript objects with name and value
-  return(
+function AddItem(props) {
+  const value = props.text; //accessing props,by default props are javascript objects with name and value
+  return (
     <form>
       <label for="text-form">Type Something: </label>
-      <input type="text" defaultValue={value} id="text-form"/>
+      <input type="text" defaultValue={value} id="text-form" />
       <p>{props.number}</p>
     </form>
   );
 }
 
-AddItem.defaultProps ={
-  number:2,
-  text:"default",
+AddItem.defaultProps = {
+  number: 2,
+  text: "default",
 }
 export default App;
