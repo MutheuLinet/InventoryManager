05c5f796122385b93935b2b3d3f173a60b8db40a
MutheuLinet <muneenilinet@gmail.com>
2022-10-24 15:39:23 +0300

    Create SearchBar functional component with a form for inputting items to search

---
+++ src/SearchBar.js
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+function SearchBar(props) {
+
+    const [name, setName] = useState("");
+    const [price, setPrice] = useState(0);
+    const [type, setType] = useState("");
+    const [brand, setBrand] = useState("");
+
+    return (
+        <div>
+            <h2>
+                Search for an Item</h2>
+            <form>
+                <label for="name-field">Name:</label>
+                <input id="name-field" type="text" value={name} />
+                <label for="price-field">Max Price:</label>
+                <input id="price-field" type="number" value={price} />
+                <label for="type-field">Type:</label>
+                <input id="type-field" type="text" value={type} />
+                <label for="brand-field">Brand:</label>
+                <input id="brand-field" type="text" value={brand} />
+                <button type="button">Search</button>
+            </form>
+        </div>
+    )
+}
+export { SearchBar };
