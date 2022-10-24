200120af20f4e4e1808e3ff0937457dbd10a071a
MutheuLinet <muneenilinet@gmail.com>
2022-10-24 16:10:29 +0300

    Add onChange event to the input fields so as th bind what is typed to the input field

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -13,13 +13,13 @@ function SearchBar(props) {
                 Search for an Item</h2>
             <form>
                 <label for="name-field">Name:</label>
-                <input id="name-field" type="text" value={name} />
+                <input id="name-field" type="text" value={name} onChange={(e) => setName(e.target.value)}  />
                 <label for="price-field">Max Price:</label>
-                <input id="price-field" type="number" value={price} />
+                <input id="price-field" type="number" value={price} onChange={(e) => setPrice(e.target.value)}/>
                 <label for="type-field">Type:</label>
-                <input id="type-field" type="text" value={type} />
+                <input id="type-field" type="text" value={type} onChange={(e) => setType(e.target.value)}/>
                 <label for="brand-field">Brand:</label>
-                <input id="brand-field" type="text" value={brand} />
+                <input id="brand-field" type="text" value={brand} onChange= {(e) => setBrand(e.target.value)}/>
                 <button type="button">Search</button>
             </form>
         </div>
