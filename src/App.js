8c9ca653b2f712ab22ce4ed3d68360984d840315
MutheuLinet <muneenilinet@gmail.com>
2022-10-25 11:54:31 +0300

    Desplay product component on the main app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
+import { Product } from './Product';
 import { SearchBar } from './SearchBar';
 
 function App() {
   return (
     <div className="App">
       <SearchBar />
+      <Product 
+      name="Amazon Echo" 
+      description="Your AI assistant" 
+      price={59.99}
+      />
     </div>
   );
 }
